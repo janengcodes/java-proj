74bb919b03c7c87aab5b4b086c76c86bf8e52b69
Jane Nguyen <janeng@umich.edu>
2024-05-16 12:50:00 -0600

    menus

---
+++ MyFrameMenus.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+public class MyFrameMenus extends JFrame implements ActionListener{
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenu editMenu;
+    JMenu helpMenu;
+
+    JMenuItem loadItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
+
+    ImageIcon loadIcon;
+
+    MyFrameMenus() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(new FlowLayout());
+
+        loadIcon = new ImageIcon("logo.png");
+
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        editMenu = new JMenu("Edit");
+        helpMenu = new JMenu("Help");
+
+        loadItem = new JMenuItem("Load");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        
+
+        loadItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        loadItem.setIcon(loadIcon);
+
+        loadItem.setMnemonic(KeyEvent.VK_L);
+        saveItem.setMnemonic(KeyEvent.VK_S);
+        exitItem.setMnemonic(KeyEvent.VK_E);
+
+        fileMenu.add(loadItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+        menuBar.add(helpMenu);
+        menuBar.add(editMenu);
+        menuBar.add(fileMenu);
+
+        this.setJMenuBar(menuBar);
+
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loadItem) {
+            System.out.println("Load");
+        }
+        if (e.getSource() == saveItem) {
+            System.out.println("Save");
+        }
+        if (e.getSource() == exitItem) {
+            System.out.println("Exit");
+            System.exit(0);
+        }
+    }
+}

8d7ee164c059a901072b8ff4367b4002c45852c0
Jane Nguyen <janeng@umich.edu>
2024-05-16 00:28:35 -0600

    j layered panes

--- JLayeredPanes.java
+++ JLayeredPanes.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Main {
+public class JLayeredPanes {
     public static void main(String[] args) {
         JLabel label1 = new JLabel();
         label1.setOpaque(true);
